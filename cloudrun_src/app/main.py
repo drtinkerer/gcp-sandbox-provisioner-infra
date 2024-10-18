dda8da8b369e2e5e3c85d1940e555d16a443c25a
Bhushan Rane <b.rane@etops.ch>
2024-10-18 16:44:55 +0530

    added slack integration

--- cloudrun_src/app/main.py
+++ cloudrun_src/app/main.py
@@ -1,40 +1,58 @@
 import os
-import json
 from fastapi import FastAPI, HTTPException
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt.app import App
+from slack_handlers import register_handlers
 from datetime import timedelta, datetime, UTC
+from base_models import SandboxCreate
+from providers.gcp import *
 from google.protobuf.timestamp_pb2 import Timestamp
 
-from app.base_models import SandboxCreate, SandboxDelete, SandboxExtend, authorized_domains, team_folders, team_names
-from app.project_manager import create_sandbox_project, delete_sandbox_project, update_project_billing_info
-from app.utils import generate_project_id, get_active_projects_count, get_cloud_task_expiry_time, delete_cloud_task, list_cloud_tasks
-from app.cloud_tasks_manager import create_deletion_task
+# Set enviornment vars
+BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 
-app = FastAPI()
+team_folders = {
+    "Team-3": "folders/550157719134",
+    "Team-4": "folders/1005335406543",
+    "Team-Data": "folders/118001772766",
+    "Team-DevOps": "folders/330608683756"
+}
 
-max_allowed_projects_per_user = int(os.environ["MAX_ALLOWED_PROJECTS_PER_USER"])
+max_allowed_projects_per_user = 1
 
-variable = "Dummy"
+# Load Slack Bolt
+app = App(token=BOT_TOKEN, signing_secret=SIGNING_SECRET)
 
-@app.post("/create_sandbox/")
-def create_sandbox(user_data: SandboxCreate):
+# Load FastAPI
+api = FastAPI()
+
+# Load Slack bolt handlers
+SocketModeHandler(app, APP_TOKEN).connect()
+
+register_handlers(app)
+
+# Load FastAPI endpoints
+@api.get("/")
+async def root():
+    return {"OK"}
+
+
+
+@api.post("/create_gcp_sandbox/")
+def create_gcp_sandbox(user_data: SandboxCreate):
     """
     Creates a google cloud sandbox project based on the provided user data.
     """
+    # return (user_data)
     user_email = user_data.user_email
     team_name = user_data.team_name
-    requested_duration_hours = user_data.requested_duration_hours
+    requested_duration_hours = int(user_data.requested_duration_hours)
     request_description = user_data.request_description
+    folder_id = team_folders[team_name]
 
     user_email_prefix = user_email.split("@")[0].replace(".", "-")
-    user_email_domain = user_email.split("@")[1]
-    
-    if user_email_domain not in authorized_domains:
-        raise HTTPException(status_code=400, detail=f"ERROR 400: User {user_email} doesnt belong to authorized domains {authorized_domains}")
-
-    if team_name not in team_names:
-        raise HTTPException(status_code=400, detail=f"ERROR 400: Provided team_name {team_name} is invalid. Required value must be one in {team_names}")
-
-    folder_id = team_folders[team_name]
 
     # Check active sandboxes
     active_projects_count = get_active_projects_count(user_email_prefix, folder_id)
@@ -42,13 +60,13 @@ def create_sandbox(user_data: SandboxCreate):
         raise HTTPException(status_code=400, detail=f"ERROR 400: User {user_email} has reached maximum number of allowed active sandbox projects.")
 
     request_time = datetime.now(UTC)
+    request_time_str = request_time.strftime("%Y-%m-%d-%H-%M-%S")
 
     delta = timedelta(hours=requested_duration_hours)
     expiry_timestamp = Timestamp()
     expiry_timestamp.FromDatetime(request_time + delta)
 
-    current_timestamp = int(request_time.timestamp())
-    project_id = generate_project_id(user_email, current_timestamp)
+    project_id = generate_project_id(user_email, request_time_str)
 
     print(f"Handling sandbox project creation event for {user_email}...")
     create_project_response = create_sandbox_project(project_id, folder_id)
@@ -61,7 +79,7 @@ def create_sandbox(user_data: SandboxCreate):
     print(f"Creating deletion task for Project {project_id} on Google Cloud Tasks queue...")
     create_deletion_task_response = create_deletion_task(project_id, project_id, expiry_timestamp)
     print(f"Successfully created deletion task for Project {project_id} on Google Cloud Tasks queue.")
-    
+
     return {
         "detail": "Sandbox project provisioned succesfully",
         "user_email": user_email,
@@ -73,69 +91,4 @@ def create_sandbox(user_data: SandboxCreate):
         "project_url": f"https://console.cloud.google.com/welcome?project={project_id}",
         "created_at": create_project_response.create_time.strftime("%Y-%d-%m %H:%M:%S UTC"),
         "expires_at": create_deletion_task_response.schedule_time.strftime("%Y-%d-%m %H:%M:%S UTC")
-    }
-
-
-@app.get('/get_env')
-def index():
-    return os.environ
-
-@app.get('/multiply')
-def multiply(a,b):
-    return{'result': int(a)*int(b)}
-
-@app.post("/delete_sandbox")
-def delete_sandbox(user_data: SandboxDelete):
-    """
-    Deletes a google cloud sandbox project based on the provided user data.
-    """
-    project_id = user_data.project_id
-
-    print(f"Handling sandbox project deletion event for {project_id}")
-    delete_sandbox_project_response = delete_sandbox_project(project_id)
-    print(f"Succssfully deleted Project {project_id}.")
-
-    return {
-        "detail": "Sandbox project deleted succesfully",
-        "project_id": project_id,
-        "deleted_at": delete_sandbox_project_response.delete_time.strftime("%Y-%d-%m %H:%M:%S UTC")
-    }
-
-
-@app.post("/extend_sandbox_duration")
-def extend_sandbox(user_data: SandboxExtend):
-    """
-    Extends the deletion/expirty of the active sandbox project.
-    This endpoint needs to be triggered before original scheduled trigger is executed.
-    """
-    project_id = user_data.project_id
-    extend_by_hours = user_data.extend_by_hours
-    cloud_tasks_queue_id = os.environ["CLOUD_TASKS_DELETION_QUEUE_ID"]
-    try:
-        task_id = list_cloud_tasks(project_id)
-    except:
-        task_id = f"{cloud_tasks_queue_id}/tasks/{project_id}"
-        
-    new_expiry_timestamp_proto = Timestamp()
-    current_expiry_timestamp = get_cloud_task_expiry_time(task_id)
-    new_expiry_timestamp_proto.FromSeconds(current_expiry_timestamp + (3600 * extend_by_hours))
-
-    # Delete old task
-    print("Deleting task")
-    delete_cloud_task_response = delete_cloud_task(task_id)
-    print("Deleting task success")
-    print(delete_cloud_task_response)
-
-    # Create new task with updated expiry time
-    print("Creating updated task with new expiry")
-    random_suffix = int(datetime.now(UTC).timestamp())
-    updated_task_name = f"{project_id}-extended-{random_suffix}"
-    create_deletion_task_response = create_deletion_task(project_id, updated_task_name, new_expiry_timestamp_proto)
-    print("Creating updated task with new expiry success")
-    print(create_deletion_task_response)
-
-    return {
-        "detail": f"Sandbox project expiry extended by {extend_by_hours} hours succesfully",
-        "project_id": project_id,
-        "new_expiry": create_deletion_task_response.schedule_time.strftime("%Y-%d-%m %H:%M:%S UTC")
     }
